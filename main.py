d36d923bb8477c6adff5b16189df187bed3cdba4
mwteusz <240759@edu.p.lodz.pl>
2023-12-16 20:58:29 +0100

    update project

--- main.py
+++ main.py
@@ -1,49 +1,107 @@
 import cv2
-import numpy as np
 import os
 
-from dataClass import image_processing
-
-
-# Funkcja do zmniejszenia rozdzielczości obrazu
-def decrease_resolution(img, step=2):
-    return None
-
-
-# Funkcja do def mirror_img(img):
-#     img = cv2.flip(img, 1)
-#     return imgobrócenia obrazu lustrzanie
-
-
-def rotate_img(img, background="avg"):
+def decrease_resolution(img, step=10):
+    img = img[::step, ::step]
     return img
 
-
-# Funkcja do zapisu obrazu o nazwie name i w folderze directory
-# pamiętaj by dodać format pliku bo inaczej się zepsuje
-def save_img(img, name: str, directory: str) -> None:
-    cdir = os.getcwd()
-    os.chdir(directory)
-    cv2.imwrite(name, img)
-    os.chdir(cdir)
-    return None
+def treshold(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    return thresh
 
 
-def rename_files_in_dir(directory: str = "img_dir") -> None:
-    temp_ = directory[directory.rfind('/')+1:]
-    for count, filename in enumerate(os.listdir(directory)):
-        format_ = filename[filename.rfind('.'):]
-        new_name = f'{temp_}_img_{count}{format_}'
-        print(f'{directory}: {filename} | {new_name}')
-        src = f'{directory}/{filename}'
-        dst = f'{directory}/{new_name}'
-        os.rename(src, dst)
-    return None
+class image_processing:
 
 
-def make_new_dir(new_dir: str, dst_dir: str) -> None:
+    def __init__(self):
+        self.folders = {}
+        self.directory_path = ""
+
+    def load_images(self, path: str):
+        self.folders = {}
+        self.directory_path = path
+
+        for folder_name in os.listdir(path):
+            images = []
+            folder_name_ = f'{path}/{folder_name}'
+            for name in os.listdir(folder_name_):
+                name = f'{folder_name_}/{name}'
+                image = cv2.imread(name)
+                image = decrease_resolution(image, 6)
+                images.append(image)
+            self.folders.update({folder_name: images})
+
+        self.print_status()
+
+    def mirror(self) -> None:
+
+        for folder_name in self.folders:
+            images = self.folders[folder_name]
+            images_flipped = []
+
+            for image in images:
+                images_flipped.append(cv2.flip(image, 1))
+
+            images.extend(images_flipped)
+
+        self.print_status()
+        return None
+
+    def print_status(self) -> None:
+        print("status:")
+        for folder_name in self.folders:
+            print(folder_name, "-", len(self.folders[folder_name]), "images")
+
+        return None
+
+    def save(self, path: str) -> None:
+        print("saving...")
+
+        for folder_name in self.folders:
+            for i, image in enumerate(self.folders[folder_name]):
+                cdir = os.getcwd()
+
+                dir = cdir + "\\" + self.directory_path + "_processed\\" + folder_name
+                filename = folder_name + "_" + str(i) + ".png"
+
+                print(dir + "\\" + filename)
+                save_img(image, dir, filename)
+
+        print("saved")
+        return None
+
+    def auto_treshold(self):
+
+        folders_tresholded = {}
+
+        for folder_name in self.folders:
+            images = self.folders[folder_name]
+
+            images_tresholded = []
+            for image in images:
+                images_tresholded.append(treshold(image))
+
+            folders_tresholded.update({folder_name: images_tresholded})
+            images.extend(images_tresholded)
+
+        self.folders = folders_tresholded
+        self.print_status()
+        return None
+
+
+def save_img(image, directory, filename) -> None:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    full_path = os.path.join(directory, filename)
+
+    cv2.imwrite(full_path, image)
+
+
+
+def make_new_dir(new_dir) -> None:
     current_dir = os.getcwd()
-    os.chdir(dst_dir)
     file_list = os.listdir()
     if new_dir not in file_list:
         os.mkdir(new_dir)
@@ -51,37 +109,13 @@ def make_new_dir(new_dir: str, dst_dir: str) -> None:
     return None
 
 
-def create_mirror_img(dir_name: str, dst_name: str) -> None:
-    for count, filename in enumerate(os.listdir(dir_name)):
-        name = f'{dir_name}/{filename}'
-        img = cv2.imread(name)
-        ind_start = filename.rfind('/')
-        ind_end = filename.rfind('.')
-        new_dir_name = f'{filename[ind_start+1:ind_end]}_mirror'
-        make_new_dir(new_dir_name, dst_name)
-        #img = mirror_img(img)
-        new_dir_name = f'{dst_name}/{new_dir_name}'
-        save_img(img, f'Test_{count}.jpg', new_dir_name)
-    return None
+def main():
+    processor = image_processing()
+    processor.load_images("data")
+    processor.auto_treshold()
+    processor.mirror()
+    processor.save("data")
 
 
-def process(rename_files: bool = True) -> None:
-    dir_name = "img_dir"
-    current_dir = os.getcwd()
-    make_new_dir("Test", current_dir)
-    test_dir = f'{current_dir}/Test'
-    for count, dirname in enumerate(os.listdir(dir_name)):
-        temp_ = f'{dir_name}/{dirname}'
-        if rename_files:
-            rename_files_in_dir(temp_)
-        create_mirror_img(temp_, test_dir)
-
-    return None
-
-
-if __name__ == "__main__":
-    processing = image_processing()
-    processing.__int__("img_dir")
-    processing.mirror()  # 2x
-    #processing.rotate()  # 4x
-    processing.save("img_dir_processed")
+if __name__ == '__main__':
+    main()
