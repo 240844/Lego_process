6f8dcb0b4047ec4e0e605d68d3883edb0d3ea5dd
KrzysztofKozlowski <krzysztofkozlowski0123@gmail.com>
2023-12-17 22:39:23 +0100

    Show labeling results

--- main.py
+++ main.py
@@ -116,6 +116,12 @@ def main():
     processor.auto_treshold()
     processor.mirror()
     processor.save("data")
+    test = cv2.imread("data\example\im.png")
+    cv2.imshow("test",test)
+    cv2.waitKey()
+    l, limage = label_image(test)
+    cv2.imshow("limage",limage)
+    cv2.waitKey()
 
 def label_image(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -124,7 +130,7 @@ def label_image(image):
     retval, thresholded = cv2.threshold(v, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     labels = skimage.measure.label(thresholded)
-    labeledImage = skimage.color.label2rgb(labels, image=rawImage, bg_label=0)
+    labeledImage = skimage.color.label2rgb(labels, image=image, bg_label=0)
     return labels, labeledImage
 
 if __name__ == '__main__':
