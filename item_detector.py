b8b2966991af5e82f08eb3d20c8efb9e9569393e
KrzysztofKozlowski <krzysztofkozlowski0123@gmail.com>
2023-12-28 00:35:48 +0100

    created function mask_object
    created function mask object that returns a sigle object from a photo based on the number of it's label

--- item_detector.py
+++ item_detector.py
@@ -16,6 +16,11 @@ def main():
     masked = mask_image(l,test)
     cv2.imshow("masked",masked)
     cv2.waitKey()
+    itemNumb = np.max(l)
+    for i in range(1, itemNumb+1):
+        item = mask_object(l, test, i)
+        cv2.imshow("item" + str(i),item)
+        cv2.waitKey()
     
 
 def label_image(image):
@@ -33,6 +38,34 @@ def mask_image(label, image):
     mask[:,:] = (label[:,:] > 0)
     mimage = cv2.bitwise_and(src1=image, src2=image,mask=mask)
     return mimage 
+
+def mask_object(label, image, item):
+    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    mask[:,:] = (label[:,:] == item)
+    mimage = cv2.bitwise_and(src1=image, src2=image,mask=mask)
+    all  = np.where(label == item)
+    xmin = all[0][0]
+    xmax = all[0][-1]
+    ymin = np.min(all[1])
+    ymax = np.max(all[1])
+
+    diff = (xmax-xmin) - (ymax-ymin)
+
+    if(diff<0):
+        #this part handles odd diffrences
+        diff = -diff
+        d = int(diff/2)
+        diff -= d
+
+        mitem = np.zeros(shape = (ymax-ymin,ymax-ymin,3),dtype = np.uint8)
+        mitem[d:ymax-ymin-diff,:,:] = mimage[xmin:xmax,ymin:ymax,:]
+    else:
+        d = int(diff/2)
+        diff -= d
+        
+        mitem = np.zeros(shape = (xmax-xmin,xmax-xmin,3),dtype = np.uint8)
+        mitem[:,d:xmax-xmin-diff,:] = mimage[xmin:xmax,ymin:ymax,:]
+    return mitem
 
 if __name__ == '__main__':
     main()
