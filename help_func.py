8a5b7e960272f209757b34536ee2557bd228a0e9
Kacper Zatoński <240844@edu.p.lodz.pl>
2024-01-02 10:04:13 +0100

    Changed zoom in and out to be functions and have scale.

--- help_func.py
+++ help_func.py
@@ -3,47 +3,32 @@ import cv2
 import numpy as np
 
 
-def decrease_resolution(img, step=10):
-    img = img[::step, ::step]
-    return img
+def decrease_resolution(image: np.ndarray, step: int = 10) -> np.ndarray:
+    image = image[::step, ::step]
+    return image
 
 
-def threshold(gray_image):
+def threshold(gray_image: np.ndarray) -> np.ndarray:
     ret, thresh = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     return thresh
 
 
-def get_avg(img):
-    output1 = np.average(img[:, :, 0])
-    output2 = np.average(img[:, :, 1])
-    output3 = np.average(img[:, :, 2])
+def get_avg(image: np.ndarray) -> list:
+    output1 = np.average(image[:, :, 0])
+    output2 = np.average(image[:, :, 1])
+    output3 = np.average(image[:, :, 2])
     output = [output1, output2, output3]
     return output
 
 
-def rotate_image(image, angle):
-    image_center = tuple(np.array(image.shape[1::-1]) / 2)
-    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-    img_avg = get_avg(result)
-    for x in range(result.shape[0]):
-        for y in range(result.shape[1]):
-            if np.array_equal([0, 0, 0], result[x, y]):
-                result[x, y] = np.asarray(img_avg)
-    return result
-
-
-def rgb_to_grayscale(image):
+def rgb_to_grayscale(image: np.ndarray) -> np.ndarray:
     b, g, r = cv2.split(image)
-
     grayscale_image = 0.299 * r + 0.587 * g + 0.114 * b
-
     grayscale_image = np.uint8(grayscale_image)
-
     return grayscale_image
 
 
-def reduce(image, num_colors=2):
+def reduce(image: np.ndarray, num_colors: int = 2) -> np.ndarray:
     pixels = image.reshape((-1, 3))
     pixels = np.float32(pixels)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
@@ -54,25 +39,23 @@ def reduce(image, num_colors=2):
     return segmented_image
 
 
-def get_darkest_color(image):
+def get_darkest_color(image: np.ndarray) -> int:
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     min_intensity_coord = np.unravel_index(np.argmin(gray_img), gray_img.shape)
     darkest_color = image[min_intensity_coord]
     return darkest_color
 
 
-def replace_color(image, old_background_color, new_background_color):
-
+def replace_color(image: np.ndarray, old_background_color: np.ndarray, new_background_color) -> np.ndarray:
     new_image = image.copy()
     for x in range(image.shape[0]):
         for y in range(image.shape[1]):
             if np.all(image[x, y] == old_background_color):
                 new_image[x, y] = new_background_color
-
     return new_image
 
 
-def save_img(image, directory, filename) -> None:
+def save_img(image: np.ndarray, directory: str, filename: str) -> None:
     if not os.path.exists(directory):
         os.makedirs(directory)
 
@@ -82,7 +65,7 @@ def save_img(image, directory, filename) -> None:
     return None
 
 
-def make_new_dir(new_dir) -> None:
+def make_new_dir(new_dir: str) -> None:
     current_dir = os.getcwd()
     file_list = os.listdir()
     if new_dir not in file_list:
--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,49 @@
-import numpy as np
-
 from help_func import *
+
+
+def rotate_image(image: np.ndarray, angle: int) -> np.ndarray:
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    img_avg = get_avg(result)
+    for x in range(result.shape[0]):
+        for y in range(result.shape[1]):
+            if np.array_equal([0, 0, 0], result[x, y]):
+                result[x, y] = np.asarray(img_avg)
+    return result
+
+
+def zoom_out(image: np.ndarray, scale: int = 50) -> np.ndarray:
+    new_image = np.zeros((image.shape[0], image.shape[1], 3))
+    img_avg = get_avg(image)
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+            new_image[x, y] = np.asarray(img_avg)
+    x_offset = int(image.shape[0] / 4)
+    y_offset = int(image.shape[1] / 4)
+    width = int(image.shape[1] * scale / 100)
+    height = int(image.shape[0] * scale / 100)
+    dim = (width, height)
+    img = cv2.resize(image, dim)
+    new_image[y_offset:y_offset + img.shape[0], x_offset:x_offset + img.shape[1]] = img
+    return new_image
+
+
+def zoom_in(image: np.ndarray, scale: int = 50) -> np.ndarray:
+    x_offset = int(image.shape[0] / 4)
+    y_offset = int(image.shape[1] / 4)
+    n_width = image.shape[0]
+    n_height = image.shape[1]
+    new_image = np.zeros((n_width, n_height, 3))
+    scale += 100
+    width = int(image.shape[1] * scale / 100)
+    height = int(image.shape[0] * scale / 100)
+    dim = (width, height)
+    img = cv2.resize(image, dim)
+    for x in range(n_width):
+        for y in range(n_height):
+            new_image[x, y] = img[x + x_offset, y + y_offset]
+    return new_image
 
 
 class ImageProcessor:
@@ -61,7 +104,7 @@ class ImageProcessor:
         print("saved")
         return None
 
-    def replace_darkest_color(self, new_color=(0, 0, 0)) -> None:
+    def replace_darkest_color(self, new_color: tuple[int, int, int] = (0, 0, 0)) -> None:
         folders_edited = {}
 
         for folder_name in self.folders:
@@ -79,7 +122,7 @@ class ImageProcessor:
         self.folders = folders_edited
         return None
 
-    def reduce_colors(self, color_amount=2) -> None:
+    def reduce_colors(self, color_amount: int = 2) -> None:
         folders_edited = {}
 
         for folder_name in self.folders:
@@ -95,9 +138,8 @@ class ImageProcessor:
         self.folders = folders_edited
         return None
 
-    def decrease_resolutions(self, param) -> None:
+    def decrease_resolutions(self, param: int) -> None:
         folders_edited = {}
-
         for folder_name in self.folders:
             images = self.folders[folder_name]
 
@@ -111,7 +153,7 @@ class ImageProcessor:
         self.folders = folders_edited
         return None
 
-    def rotate_images(self, angle=20, r=1) -> None:
+    def rotate_images(self, angle: int = 20, r: int = 1) -> None:
         for folder_name in self.folders:
             images = self.folders[folder_name]
             images_rotated = []
@@ -125,47 +167,24 @@ class ImageProcessor:
         self.print_status()
         return None
 
-    def zoom_out(self, image):
-        new_image = np.zeros((image.shape[0], image.shape[1], 3))
-        img_avg = get_avg(image)
-        for x in range(image.shape[0]):
-            for y in range(image.shape[1]):
-                new_image[x, y] = np.asarray(img_avg)
-        x_offset = int(image.shape[0] / 4)
-        y_offset = int(image.shape[1] / 4)
-        img = decrease_resolution(image, step=2)
-        new_image[y_offset:y_offset + img.shape[0], x_offset:x_offset + img.shape[1]] = img
-        return new_image
-
-    def zoom_in(self, image):
-        x_offset = int(image.shape[0] / 4)
-        y_offset = int(image.shape[1] / 4)
-        n_width = int(image.shape[0] / 2)
-        n_height = int(image.shape[1] / 2)
-        new_image = np.zeros((n_width, n_height, 3))
-        for x in range(n_width):
-            for y in range(n_height):
-                print(x)
-                new_image[x, y] = image[x + x_offset, y + y_offset]
-        return new_image
-
-    def zoom_out_all(self) -> None:
+    def zoom_all(self) -> None:
         for folder_name in self.folders:
             images = self.folders[folder_name]
             images_zoomed = []
-
             for image in images:
-                images_zoomed.append(self.zoom_out(image))
-
+                images_zoomed.append(zoom_out(image, scale=60))
+                images_zoomed.append(zoom_out(image))
+                images_zoomed.append(zoom_in(image))
             images.extend(images_zoomed)
-
         self.print_status()
         return None
+
 
 def main():
     processor = ImageProcessor()
     processor.load_images("data")
-    processor.rotate_images(20, 1)
+    #processor.rotate_images(20, 1)
+    processor.zoom_all()
     processor.decrease_resolutions(3)  # 168x168 * (1/3) = 56x56
     processor.reduce_colors(color_amount=2)
     processor.replace_darkest_color(new_color=(0, 0, 0))  # zamienia tlo na czarne
